876241112824f729c06d1668e3a16ba12ce6af44
luizgbraga <luizb.07@outlook.com>
2022-10-14 16:36:44 -0300

    little adjust

--- welcome.php
+++ welcome.php
@@ -7,7 +7,7 @@ session_start();
 require_once("app/app.php");
  
 // verifique se está logado; senão, redirecione para o login
-ensure_user_is_authenticated();
+// ensure_user_is_authenticated();
 
 class Consumo {
     public $data;
