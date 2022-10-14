abbdcca77d6a2444ef933a69bd030dd14c1f4ae4
luizgbraga <luizb.07@outlook.com>
2022-10-14 17:08:31 -0300

    better colors

--- welcome.php
+++ welcome.php
@@ -7,11 +7,38 @@ session_start();
 require_once("app/app.php");
  
 // verifique se está logado; senão, redirecione para o login
-// ensure_user_is_authenticated();
+ensure_user_is_authenticated();
 
 class Consumo {
     public $data;
     public $consumo;
+}
+
+$user_inputs = (array) Data::get_inputs($_SESSION['id']);
+$subordinados = Data::get_subordinados($_SESSION['id']);
+
+$subordinados_inputs = [];
+
+$datas = [];
+$consumo = [];
+
+
+foreach($subordinados as $subordinado) {
+    $inputs = (array) Data::get_inputs($subordinado->id);
+    $subordinados_inputs = [...$subordinados_inputs, $inputs];
+}
+
+foreach($user_inputs as $input) {
+    $datas = [...$datas, $input->data];
+    $consumo = [...$consumo, $input->dados->consumo];
+}
+
+$subordinados_consumo = [];
+foreach($subordinados_inputs as $input) {
+    $consumo_sub = new Consumo();
+    $consumo_sub->data = $input->data;
+    $consumo_sub->consumo = $input->dados->consumo;
+    $subordinados_consumo = [...$subordinados_consumo, $consumo_sub];
 }
 
 include('./views/welcome.view.php');
