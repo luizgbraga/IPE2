d8996261679c022a12560792cf86ef050b0f33ab
luizgbraga <luizb.07@outlook.com>
2022-10-19 16:26:59 -0300

    table

--- gerenciar.php
+++ gerenciar.php
@@ -6,7 +6,7 @@ session_start();
 require_once("app/app.php");
 
 // verifique se está logado; senão, redirecione para o login
-ensure_user_is_authenticated();
+
 
 $user_inputs = (array) Data::get_inputs($_SESSION['id']);
 
--- views/gerenciar.view.php
+++ views/gerenciar.view.php
@@ -11,6 +11,7 @@
             <?php include 'styles/default.style.css'; ?>
             <?php include 'styles/header.style.css'; ?>
             <?php include 'styles/nav.style.css'; ?>
+            <?php include 'styles/gerenciar.style.css'; ?>
             <?php include 'styles/modal.style.css'; ?>
         </style>
 
@@ -30,6 +31,40 @@
             <div class='column'>
 
                 <h2 class='main-title-page'>Gerenciar Inputs</h2>
+
+                <div class='content'>
+
+                    <table id="customers">
+                        <tr>
+                            <th>Data</th>
+                            <th>Consumo</th>
+                            <th>Demanda medida</th>
+                            <th>Energia Ativa</th>
+                            <th>Energia Reativa</th>
+                        </tr>
+                        <tr>
+                            <?php
+
+                                foreach($user_inputs as $input) {
+                                    $data = $input->data;
+                                    $consumo = $input->dados->consumo;
+                                    $demanda_medida = $input->dados->demanda_medida;
+                                    $energia_ativa = $input->dados->energia_ativa;
+                                    $energia_reativa = $input->dados->energia_reativa;
+                                    echo "
+                                        <td>$data</td>
+                                        <td>$consumo</td>
+                                        <td>$demanda_medida</td>
+                                        <td>$energia_ativa</td>
+                                        <td>$energia_reativa</td>
+                                    "
+                                }
+
+                            ?>
+                        </tr>
+                    </table>
+                
+                </div>
 
                 <?php include 'modal.view.php'; ?>
 
